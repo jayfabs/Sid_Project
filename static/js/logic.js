d314f9dca72a2b8859d091fa769260cac696b665
John Fabricatore <94406047+jayfabs@users.noreply.github.com>
2023-05-26 22:00:41 -0400

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,32 +1,35 @@
 function loadFile() {
-    // Get the file input element
-    var fileInput = document.getElementById("fileInput");
-  
-    // Check if a file is selected
-    if (fileInput.files.length > 0) {
-      var file = fileInput.files[0]; // Get the first file from the list
-  
-      var reader = new FileReader();
-  
-      // Set up an event handler for when the file has been loaded
-      reader.onload = function(event) {
-        // Retrieve the file contents
-        var fileContents = event.target.result;
-  
-        // Perform any processing or manipulation on the file contents here
-  
-        // Save the file to the "data" folder
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", "./data/" + file.name, true);
-        xhr.setRequestHeader("Content-Type", "application/octet-stream");
-        xhr.send(fileContents);
-  
-        // Display a message or perform any additional actions
-        console.log("File saved successfully!");
-      };
-  
-      // Read the file as binary data
-      reader.readAsBinaryString(file);
-    }
+  // Get the file input element
+  var fileInput = document.getElementById("fileInput");
+
+  // Check if a file is selected
+  if (fileInput.files.length > 0) {
+    var file = fileInput.files[0]; // Get the first file from the list
+
+    var reader = new FileReader();
+
+    // Set up an event handler for when the file has been loaded
+    reader.onload = function(event) {
+      // Retrieve the file contents
+      var fileContents = event.target.result;
+
+      // Perform any processing or manipulation on the file contents here
+
+      // Save the file to the "data" folder
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "./data/" + file.name, true);
+      xhr.setRequestHeader("Content-Type", "application/octet-stream");
+      xhr.send(fileContents);
+
+      // Display the file contents
+      console.log("File contents:");
+      console.log(fileContents);
+
+      // Display a message or perform any additional actions
+      console.log("File saved successfully!");
+    };
+
+    // Read the file as text
+    reader.readAsText(file);
   }
-  
+}
